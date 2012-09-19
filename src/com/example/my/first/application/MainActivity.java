3505f18cfbdc466d4d8d65f4f10838e2f2a58ff2
cindyshen <cindyshen99@hotmail.com>
2012-09-19 12:12:25 -0400

    My First Android Application

---
+++ src/com/example/my/first/application/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.my.first.application;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("http://syndicateme.net/jobsearch");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
